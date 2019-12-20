31e7df124f149d9ff657b450f7bb2385f8187912
Emanuel Oliveira <emanuel.foliveira@gmail.com>
2019-12-20 15:33:00 -0300

    Creating Project Structure

---
+++ src/main/java/br/com/efo/zipcodesearcher/ZipcodeSearcherApplication.java
@@ -0,0 +1,13 @@
+package br.com.efo.zipcodesearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodeSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodeSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/efo/zipcodesearcher/ZipcodeSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.efo.zipcodesearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodeSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
