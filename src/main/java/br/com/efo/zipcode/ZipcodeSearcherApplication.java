7ac6b1e926f87fe18f029137275bdea5e628bd53
Emanuel Oliveira <emanuel.foliveira@gmail.com>
2019-12-26 10:47:51 -0300

    Finetune on files

--- src/main/java/br/com/efo/zipcode/ZipcodeSearcherApplication.java
+++ src/main/java/br/com/efo/zipcode/ZipcodeSearcherApplication.java
@@ -1,4 +1,4 @@
-package br.com.efo.zipcode.main;
+package br.com.efo.zipcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/efo/zipcode/controller/ZipcodeController.java
+++ src/main/java/br/com/efo/zipcode/controller/ZipcodeController.java
@@ -3,24 +3,20 @@ package br.com.efo.zipcode.controller;
 import br.com.efo.zipcode.model.response.ZipcodeResponse;
 import br.com.efo.zipcode.service.IZipcodeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/zipcode")
 public class ZipcodeController {
 
     @Autowired
     private IZipcodeService service;
 
     @GetMapping(value = "/search")
-    public ResponseEntity<ZipcodeResponse> search(final String zipcode) {
-        return null;
+    public ZipcodeResponse search() {
+        return service.searchZipcode("");
     }
 
 
